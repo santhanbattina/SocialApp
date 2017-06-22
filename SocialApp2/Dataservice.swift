1866feeeb9f038a2a99b11582a5fdd2023f49bf6
naren puppala <narenpuppala@narens-MacBook-Pro.local>
2017-06-22 13:22:17 -0700

    completed database users

---
+++ SocialApp2/Dataservice.swift
@@ -0,0 +1,14 @@
+//
+//  Data services.swift
+//  SocialApp2
+//
+//  Created by naren puppala on 6/21/17.
+//  Copyright © 2017 naren. All rights reserved.
+//
+
+import Foundation
+
+class Dataservice {
+    
+    
+}
---
+++ SocialApp2/FeedCell.swift
@@ -0,0 +1,24 @@
+//
+//  FeedCell.swift
+//  SocialApp2
+//
+//  Created by naren puppala on 6/7/17.
+//  Copyright © 2017 naren. All rights reserved.
+//
+
+import UIKit
+
+class FeedCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
